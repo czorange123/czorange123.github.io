ea2d4cff2e1da81d98094b23b7318340c4632edb
zcheng <1451426471@qq.com>
2020-05-21 19:49:43 +0800

    move js in ejs to js

---
+++ themes/oranges/source/js/activeNav.js
@@ -0,0 +1,9 @@
+
+let navs = document.querySelectorAll('.nav-item');
+let pagePath = window.location.pathname;
+for(let nav of navs) {
+  let navPath = nav.getAttribute("data-path");
+  if(navPath && navPath === pagePath) {
+    nav.className = "nav-item active";
+  }
+}
